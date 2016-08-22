c528a66b9ff35806575d1a0ea075e55bcdbdf1ef
Masaru Morita <masamorobo@gmail.com>
2016-08-22 21:37:44 +0900

    [add] python sample for visual recognition

---
+++ python_sample/classify.py
@@ -0,0 +1,21 @@
+import json
+from os.path import join, dirname
+from os import environ
+from watson_developer_cloud import VisualRecognitionV3
+
+
+def main():
+    visual_recognition = VisualRecognitionV3('2016-05-20', api_key='eff6ee0aaf5980914180ba2df0a8e220fd854b7c')
+
+    print('Start Classifying')
+
+    with open(join(dirname(__file__), '../resources/dogs.jpg'), 'rb') as dogs:
+
+        print(json.dumps(visual_recognition.classify(
+            images_file=dogs,  threshold=0.1, classifier_ids=['dogs_1474036614', 'default']), indent=2))
+
+    print('Finish Classifying')
+
+
+if __name__ == '__main__':
+        main()
---
+++ python_sample/create_classifier.py
@@ -0,0 +1,27 @@
+import json
+from os.path import join, dirname
+from os import environ
+from watson_developer_cloud import VisualRecognitionV3
+
+
+def main():
+    visual_recognition = VisualRecognitionV3('2016-05-20', api_key='eff6ee0aaf5980914180ba2df0a8e220fd854b7c')
+
+    #print('Delete Existing Classifier')
+    #print(json.dumps(visual_recognition.delete_classifier(classifier_id='dogs_1780711670'), indent=2))
+
+    print('Start Creating Classifier')
+    with open(join(dirname(__file__), '../resources/beagle.zip'), 'rb') as beagle, \
+            open(join(dirname(__file__), '../resources/husky.zip'), 'rb') as husky, \
+            open(join(dirname(__file__), '../resources/golden-retriever.zip'), 'rb') as golden_retriever, \
+            open(join(dirname(__file__), '../resources/cats.zip'), 'rb') as cats:
+        print(json.dumps(
+            visual_recognition.create_classifier('dogs',
+                                                 beagle_positive_examples=beagle,
+                                                 husky_positive_example=husky,
+                                                 goldenretriever_positive_examples=golden_retriever,
+                                                 negative_examples=cats), indent=2))
+    print('Finish Creating Classifier')
+
+if __name__ == '__main__':
+        main()
