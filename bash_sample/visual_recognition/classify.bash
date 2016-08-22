46d7f3373299b6674bfc5f5a4bc0402699443950
Masaru Morita <masamorobo@gmail.com>
2016-08-22 21:52:59 +0900

    [add] add bash sample using curl

---
+++ bash_sample/visual_recognition/classify.bash
@@ -0,0 +1,1 @@
+curl -X POST -F "images_file=@../../resources/dogs.jpg" -F "parameters=@myparams.json" "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=eff6ee0aaf5980914180ba2df0a8e220fd854b7c&version=2016-05-20"
