77feabaf91341d20be649c5a280da26f52c49440
Masaru Morita <masamorobo@gmail.com>
2016-08-28 11:11:06 +0900

    [fix] visual recognition なしver

--- python_sample/visual_recognition/classify_curl.py
+++ python_sample/visual_recognition/classify_curl.py
@@ -1,11 +1,8 @@
 import json
 from os.path import join, dirname
-from watson_developer_cloud import VisualRecognitionV3
 import commands
 
 def main():
-    visual_recognition = VisualRecognitionV3('2016-05-20', api_key='{your_key}')
-
     print('Start Classifying')
 
     cmd = commands.getoutput("../../bash_sample/visual_recognition/classify.bash > result.json")
