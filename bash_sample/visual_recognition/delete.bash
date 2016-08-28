6589a96b52b1cb29abca7e2bb7a433faf2f3d3cc
Masaru Morita <masamorobo@gmail.com>
2016-08-28 13:48:25 +0900

    Create delete.bash

---
+++ bash_sample/visual_recognition/delete.bash
@@ -0,0 +1,1 @@
+curl -X DELETE "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers/chip_or_koala_396769044?api_key=eff6ee0aaf5980914180ba2df0a8e220fd854b7c&version=2016-05-20"
